43880bdb0e63e1c2a007e64d1b29fcf2d964acfb
flori <florian.raux10@gmail.com>
2025-11-28 12:15:51 +0100

    Added vehicle GET, POST and DELETE

--- app/crud.py
+++ app/crud.py
@@ -1,8 +1,8 @@
 from sqlalchemy.orm import Session
 from app import models, schemas, utils
-from datetime import datetime
+from typing import List, Optional
 
-# === USER MANAGEMENT ===
+# === USER ===
 
 def get_user_by_email(db: Session, email: str):
     return db.query(models.User).filter(models.User.email == email).first()
@@ -24,3 +24,36 @@ def create_user(db: Session, user: schemas.UserCreate):
     db.commit()
     db.refresh(db_user)
     return db_user
+
+# === VEHICLE ===
+
+def create_vehicle(db: Session, vehicle: schemas.VehicleCreate, driver_id: int):
+    """Create a new vehicle associated with the given driver_id."""
+    db_vehicle = models.Vehicle(
+        driver_id=driver_id,
+        license_plate=vehicle.license_plate,
+        model=vehicle.model,
+        color=vehicle.color,
+        max_seats=vehicle.max_seats,
+    )
+    db.add(db_vehicle)
+    db.commit()
+    db.refresh(db_vehicle)
+    return db_vehicle
+
+def get_vehicles_by_user(db: Session, driver_id: int) -> List[models.Vehicle]:
+    """Return all vehicles owned by a specific user."""
+    return db.query(models.Vehicle).filter(models.Vehicle.driver_id == driver_id).all()
+
+def get_vehicle_by_id_and_owner(db: Session, vehicle_id: int, driver_id: int) -> Optional[models.Vehicle]:
+    """Retrieve a specific vehicle only if it belongs to the given user."""
+    return db.query(models.Vehicle).filter(
+        models.Vehicle.vehicle_id == vehicle_id,
+        models.Vehicle.driver_id == driver_id
+    ).first()
+
+def delete_vehicle(db: Session, vehicle: models.Vehicle):
+    """Delete a specific vehicle."""
+    db.delete(vehicle)
+    db.commit()
+    return {"message": "Vehicle successfully deleted"}
--- app/main.py
+++ app/main.py
@@ -1,17 +1,18 @@
 from fastapi import FastAPI, Depends
-from app.routers import auth
+from app.routers import auth, vehicles
 from app import deps, schemas
 
 app = FastAPI(title="GoCampus API")
 
 # Inclusion du routeur d'authentification
 app.include_router(auth.router)
+app.include_router(vehicles.router)
 
 @app.get("/")
 def read_root():
     return {"message": "Bienvenue sur l'API GoCampus !"}
 
-# Test route
+# Users/me route
 @app.get("/users/me", response_model=schemas.UserOut)
 def read_users_me(current_user: schemas.UserOut = Depends(deps.get_current_user)):
     return current_user
